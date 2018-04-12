85f0c92ca6302273df505da38a198aac2026b370
meserkatie <37908336+meserkatie@users.noreply.github.com>
2018-04-12 18:58:36 -0500

    Add files via upload

---
+++ LoginActivity.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+namespace MailedMixers.Activities
+{
+   
+    [Activity(Label = "Login Activity", MainLauncher = true)]
+    public class LoginActivity : Activity
+    {
+        EditText txtEmail;
+        EditText txtPassword;
+        Button btnLogin;
+        Button btnRegister;
+       // ImageView IVlogo;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Login);
+            // Create your application here
+            txtEmail = FindViewById<EditText>(Resource.Id.txtEmail);
+            txtPassword = FindViewById<EditText>(Resource.Id.txtPassword);
+            btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
+            btnRegister = FindViewById<Button>(Resource.Id.btnRegister);
+           // IVlogo = FindViewById<ImageView>(Resource.Id.IVLogo);
+
+            btnLogin.Click += BtnLogin_Click;
+            btnRegister.Click += BtnRegister_Click;
+        }
+
+        private void BtnRegister_Click(object sender, EventArgs e)
+        {
+            //StartActivity(typeof(RegisterActivity));
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string dpPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "user.db3"); //Call Database  
+                var db = new SQLiteConnection(dpPath);
+                var data = db.Table<LoginTable>(); //Call Table  
+                var data1 = data.Where(x => x.username == txtEmail.Text && x.password == txtPassword.Text).FirstOrDefault(); //Linq Query  
+                if (data1 != null)
+                {
+                    Toast.MakeText(this, "Login Success", ToastLength.Short).Show();
+                }
+                else
+                {
+                    Toast.MakeText(this, "Username or Password invalid", ToastLength.Short).Show();
+                }
+            }
+            catch (Exception ex)
+            {
+                Toast.MakeText(this, ex.ToString(), ToastLength.Short).Show();
+            }
+        }
+
+        public string CreateDB()
+        {
+            var output = "";
+            output += "Creating Database if it doesnt exists";
+            string dpPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "user.db3"); //Create New Database  
+            var db = new SQLiteConnection(dpPath);
+            output += "\n Database Created....";
+            return output;
+        }
+    }
+}
---
+++ LoginTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+namespace MailedMixers
+{
+    class LoginTable
+    {
+        [PrimaryKey, AutoIncrement, Column("_UserID")]
+        public int userid { get; set; }
+        [MaxLength(25)]
+        public string username { get; set; }
+        [MaxLength(15)]
+        public string password { get; set; }
+    }
+}
